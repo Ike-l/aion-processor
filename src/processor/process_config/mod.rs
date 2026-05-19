b75376a70bff11f3c33e5471d970f4ed40472b0d
isaac <isaacloff@gmail.com>
2026-05-19 15:04:29 +0100

    Change: Removed unwinder & ProcessConfig

--- src/processor/process_config/mod.rs
+++
@@ -1,9 +0,0 @@
-use std::sync::Arc;
-
-use threadpool::ThreadPool;
-use tokio::runtime::Runtime;
-
-pub struct ProcessConfig<'a> {
-    pub runtime: Option<&'a Arc<Runtime>>,
-    pub threadpool: Option<&'a ThreadPool>,
-}
